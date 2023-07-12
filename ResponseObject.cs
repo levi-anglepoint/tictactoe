5e4bd1dd56a0b329782a8b7716c6a5e09c0307ed
Emma Swalberg <emma.swalberg@anglepoint.com>
2023-07-12 11:23:55 -0600

    Added response object

---
+++ ResponseObject.cs
@@ -0,0 +1,20 @@
+﻿namespace TicTacToeBot_EmmaLevi
+{
+    internal class ResponseObject
+    {
+        public string? PlayerXName { get; set; }
+        public string? PlayerOName { get; set; }
+        public string? RoomCode { get; set; }
+        public int CurrentGameStatus { get; set; }
+        public string? WinnerStatus { get; set; }
+        public string? CurrentTauntFromX { get; set; }
+        public string? CurrentTauntFromO { get; set; }
+        public int PlayerXVictoryCount { get; set; }
+        public int PlayerOVictoryCount { get; set; }
+        public int PlayerXForfeitCount { get; set; }
+        public int PlayerOForfeitCount { get; set; }
+        public int TieGameCount { get; set; }
+        public int CurrentRound { get; set; }
+        public char[][] GameBoard { get; set; }
+    }
+}
