bdd27447a48683f70b0b0377900b14cb803da204
Levi Thorpe <levi.thorpe@anglepoint.com>
2023-07-12 11:06:10 -0600

    first commit

---
+++ APICaller.cs
@@ -0,0 +1,63 @@
+﻿using System.Net;
+using System.Net.Http.Headers;
+
+namespace TicTacToeBot_EmmaLevi
+{
+    public class APICaller
+    {
+        private HttpClient client;
+
+        public async Task<string> PostGenerate(string endpoint)
+        {
+            client = new HttpClient();
+
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json")); // text/plain // application/json
+
+            var baseUrl = "https://localhost:7046/";
+            string url = baseUrl + endpoint;
+
+            var res = await client.PostAsync(url, null);
+            var json = await res.Content.ReadAsStringAsync();
+
+            Console.Write(json);
+
+            return json;
+        }
+
+        public async Task<string> Get(string endpoint)
+        {
+            client = new HttpClient();
+
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json")); // text/plain // application/json
+
+            var baseUrl = "https://localhost:7046/";
+            string url = baseUrl + endpoint;
+
+            var json = await client.GetStringAsync(url);
+
+            Console.Write(json);
+
+            return json;
+        }
+
+        public async Task<string> Put(string endpoint)
+        {
+            client = new HttpClient();
+
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json")); // text/plain // application/json
+
+            var baseUrl = "https://localhost:7046/";
+            string url = baseUrl + endpoint;
+
+            var res = await client.PutAsync(url, null);
+            var json = await res.Content.ReadAsStringAsync();
+
+            Console.Write(json);
+
+            return json;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using TicTacToeBot_EmmaLevi;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+string playerName = "Emvi"; // green with Emvi
+bool validInput = false;
+APICaller apiCaller = new();
+
+string playerXorO = "y";
+
+while (validInput == false)
+{
+    Console.WriteLine("Generate or Join game:");
+    string generateOrJoin = Console.ReadLine();
+
+    if (generateOrJoin == null)
+    {
+        generateOrJoin = Console.ReadLine();
+    }
+
+    if (generateOrJoin == "generate" || generateOrJoin == "g")
+    {
+        validInput = true;
+
+        // send generate api call
+        var response = await apiCaller.PostGenerate($"GenerateGame/{playerName}");
+    }
+    else if (generateOrJoin == "join" || generateOrJoin == "j")
+    {
+        validInput = true;
+        Console.WriteLine("Enter room code:");
+        string roomCode = Console.ReadLine();
+
+        // send join api call
+        var response = await apiCaller.Put($"JoinGame/{roomCode}/{playerName}");
+    }
+}
+
+
+
+
+
